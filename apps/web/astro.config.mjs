4a417115a63235909e006997075f639bdfa44123
Nouxx <clement.vannouque@gmail.com>
2026-07-27 15:28:24 +0200

    bump to astro 7

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -8,7 +8,9 @@ const isPreview = process.env.PREVIEW === "true";
 
 export default defineConfig({
   output: isPreview ? "server" : "static",
-  adapter: isPreview ? cloudflare() : undefined,
+  // output directory differs to prevent a build erasing the other
+  outDir: isPreview ? "./dist/preview" : "./dist/static",
+  adapter: isPreview ? cloudflare({ imageService: "passthrough" }) : undefined,
   fonts: [
     {
       provider: fontProviders.google(),
