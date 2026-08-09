55d35a59d3b90a864e3731744564649ffd173451
Nouxx <clement.vannouque@gmail.com>
2026-08-09 16:03:55 +0200

    wip section

--- packages/ui/src/types.ts
+++ packages/ui/src/types.ts
@@ -1,3 +1,17 @@
+export type SpaceScale =
+  | "1"
+  | "2"
+  | "3"
+  | "4"
+  | "5"
+  | "6"
+  | "8"
+  | "10"
+  | "12"
+  | "16"
+  | "20"
+  | "24";
+
 export type ImageSource = {
   url: string;
   width: number;
